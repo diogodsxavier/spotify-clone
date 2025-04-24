c019ed9be4128127d6941cce2335b869efd4ea80
Diogo Xavier <diogodsxavier@gmail.com>
2025-04-24 14:16:25 -0300

    Update deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ApiResponse, Track } from "./types/spotify";
 import { useAuth } from "./hooks/useAuth";
@@ -70,57 +70,61 @@ function App() {
   };
 
   const loginToSpotify = () => {
-    const redirectUri = encodeURIComponent(import.meta.env.VITE_SPOTIFY_REDIRECT_URI);
+    const redirectUri =
+      import.meta.env.MODE === "production"
+        ? "https://spotify-clone-self-ten.vercel.app/callback" // Ambiente de produção
+        : "http://localhost:5173/callback"; // Ambiente local
+
     const scopes = "user-read-private user-read-email";
 
-    const url = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${redirectUri}&scope=${scopes}&response_type=token`;
+    const url = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&redirect_uri=${encodeURIComponent(
+      redirectUri
+    )}&scope=${scopes}&response_type=token`;
     console.log("URL de autenticação gerada:", url);
 
     window.location.href = url;
   };
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 relative">
-              {!token ? (
-                <button
-                  className="px-4 py-2 bg-green-500 text-white rounded-full font-bold text-lg transition duration-300 hover:bg-green-600"
-                  onClick={loginToSpotify}
-                >
-                  Login com Spotify
-                </button>
-              ) : (
-                <>
-                  <div className="w-full max-w-md px-4 mt-8">
-                    <SearchBar onSearch={handleSearch} />
-                  </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 relative">
+            {!token ? (
+              <button
+                className="px-4 py-2 bg-green-500 text-white rounded-full font-bold text-lg transition duration-300 hover:bg-green-600"
+                onClick={loginToSpotify}
+              >
+                Login com Spotify
+              </button>
+            ) : (
+              <>
+                <div className="w-full max-w-md px-4 mt-8">
+                  <SearchBar onSearch={handleSearch} />
+                </div>
 
-                  <TrackList
-                    tracks={tracks}
-                    onPlay={(url) => setCurrentTrack(url || "")}
-                  />
-                  <Player previewUrl={currentTrack} />
+                <TrackList
+                  tracks={tracks}
+                  onPlay={(url) => setCurrentTrack(url || "")}
+                />
+                <Player previewUrl={currentTrack} />
 
-                  <div className="mt-8 w-full flex justify-center mb-8">
-                    <button
-                      onClick={logout}
-                      className="px-4 py-2 bg-red-500 text-white rounded-full font-bold text-sm md:text-lg transition duration-300 hover:bg-red-600"
-                    >
-                      Logout
-                    </button>
-                  </div>
-                </>
-              )}
-            </div>
-          }
-        />
-        <Route path="/callback" element={<Callback />} />
-      </Routes>
-    </Router>
+                <div className="mt-8 w-full flex justify-center mb-8">
+                  <button
+                    onClick={logout}
+                    className="px-4 py-2 bg-red-500 text-white rounded-full font-bold text-sm md:text-lg transition duration-300 hover:bg-red-600"
+                  >
+                    Logout
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+        }
+      />
+      <Route path="/callback" element={<Callback />} />
+    </Routes>
   );
 }
 
